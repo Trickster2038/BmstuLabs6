4eaf8b2ef5961fc529d0faf88c1adfadd876d755
win10vb <fzastahov@gmail.com>
2022-02-16 16:46:06 +0300

    init

---
+++ mk/lab1/proj/lab12/lab1.asm
@@ -0,0 +1,49 @@
+.include "m8515def.inc"
+.def temp = r16 
+.def reg_led = r20 
+.equ START = 0 
+
+.equ STOP = 1 
+.org $000
+rjmp init
+
+INIT: ldi reg_led,0x7F 
+sec 
+clt
+ser temp 
+out DDRA,temp 
+out PORTA,temp 
+clr temp  
+out DDRD,temp
+ldi temp,0x03 
+out PORTD,temp 
+
+WAITSTART: 
+sbic PIND,START 
+rjmp WAITSTART 
+
+LOOP: out PORTA,reg_led 
+ldi r17, 210
+	d1: ldi r18,255
+		d2: ldi r19, 4
+			d3: dec r19
+			brne d3
+		dec r18
+		brne d2
+	dec r17
+	brne d1
+
+sbic PIND,STOP 
+rjmp MM 
+rjmp WAITSTART 
+
+MM: brts LEFT 
+sbrs reg_led,1 
+set 
+ror reg_led 
+rjmp LOOP 
+
+LEFT: sbrs reg_led,6 
+clt 
+rol reg_led 
+rjmp LOOP
